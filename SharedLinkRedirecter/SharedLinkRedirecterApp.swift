ed0f600442cb5a4bea8c973a94824c2f3e68adf3
PacoRahn <paco.rahn@gmail.com>
2024-08-06 19:31:37 +0200

    Initial Commit

---
+++ SharedLinkRedirecter/SharedLinkRedirecterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedLinkRedirecterApp.swift
+//  SharedLinkRedirecter
+//
+//  Created by Paco Rahn on 06.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SharedLinkRedirecterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
